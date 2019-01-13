289cd917314a6cedc772625accdb3d90d7d790fd
Shubham Verma <Shubham Verma@DESKTOP-KVV1592>
2019-01-14 00:36:43 +0530

    I am pushing the yt framework assignment

---
+++ YTFramework/src/org/testing/TestScripts/TC3.java
@@ -0,0 +1,26 @@
+package org.testing.TestScripts;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TC3 {
+
+	@BeforeMethod
+	public void browserInitiate(){
+		
+		
+	}
+	
+	@Test
+	public void firstTestCase(){
+		
+		
+	}
+	
+	@AfterMethod
+	public void browserClose(){
+		
+		
+	}
+}
---
+++ YTFramework/src/org/testing/TestScripts/TC4.java
@@ -0,0 +1,27 @@
+package org.testing.TestScripts;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TC4 {
+	
+	@BeforeMethod
+	public void browserInitiate(){
+		
+		
+	}
+	
+	@Test
+	public void firstTestCase(){
+		
+		
+	}
+	
+	@AfterMethod
+	public void browserClose(){
+		
+		
+	}
+
+}
