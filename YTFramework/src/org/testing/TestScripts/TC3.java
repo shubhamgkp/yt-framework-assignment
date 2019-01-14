0ca30a496551454055cdd13bac508e69ad18ceab
Shubham Verma <Shubham Verma@DESKTOP-KVV1592>
2019-01-14 14:47:19 +0530

    I am updaing the code.....

--- YTFramework/src/org/testing/TestScripts/TC3.java
+++ YTFramework/src/org/testing/TestScripts/TC3.java
@@ -1,26 +1,67 @@
 package org.testing.TestScripts;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TC3 {
 
+	WebDriver driver;
 	@BeforeMethod
 	public void browserInitiate(){
-		
-		
+
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/Driver/chromedriver.exe");
+
+		//Code to Switch off the browser notification
+		//Create prefs map to store all preferences
+		HashMap<String, Object> prefs = new HashMap<String, Object>();		
+		//Put this into prefs map to switch off browser notification
+		prefs.put("profile.default_content_setting_values.notifications", 2);	
+		//Create chrome options to set this prefs
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("prefs", prefs);
+
+		driver = new ChromeDriver(options);
+		driver.navigate().to("https://www.youtube.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);	
 	}
-	
+
 	@Test
-	public void firstTestCase(){
+	public void thirdTestCase(){
+
+
+		driver.findElement(By.xpath("//yt-formatted-string[text()='Sign in']")).click();
+		driver.findElement(By.xpath("//input[@id='identifierId']")).sendKeys("shubhamcert@gmail.com");
+		driver.findElement(By.xpath("//div[@id='identifierNext']")).click();
+		try {			
+			driver.findElement(By.xpath("//input[@type='password']")).sendKeys("Skv@2580");
+		}
+		catch(org.openqa.selenium.StaleElementReferenceException ex)
+		{
+			driver.findElement(By.xpath("//input[@type='password']")).sendKeys("Skv@2580");
+		}		
+		driver.findElement(By.xpath("//div[@id='passwordNext']")).click();	
 		
+		List<WebElement> elements = driver.findElements(By.id("video-title"));
+		System.out.println(elements.size());
+		elements.get(0).click();		
 		
+		List<WebElement> likeDislike = driver.findElements(By.xpath("//button[contains(@aria-label,'like this video along with')]"));		
+		likeDislike.get(0).click();
 	}
-	
-	@AfterMethod
+
+	/*@AfterMethod
 	public void browserClose(){
-		
-		
-	}
+		driver.close();
+	}*/
 }
